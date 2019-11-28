c123f095c057ff38a9b5e9595a8544ab3af6792d
Anne-Louise Darko <ms.darko@live.co.uk>
2019-11-28 16:27:55 +0000

    another update

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+=begin
+ 
 students = [
  {name: "Dr. Hannibal Lecter", cohort: :november},
  {name: "Darth Vader", cohort: :november},
@@ -11,6 +13,8 @@ students = [
  {name: "Joffrey Baratheon", cohort: :november},
  {name: "Norman Bates", cohort: :november}
 ]
+
+=end
 
 def print_header
 puts "The students of Villians Academy"
@@ -27,6 +31,21 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+
+def input_students
+  puts "Please enter the name of student"
+  puts "To finish, hit enter twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students  
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
